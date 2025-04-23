11c947965ca8cc4530722c012852dc1d5f1173f8
selim <selimb768@gmail.com>
2025-04-23 12:50:29 +0300

    feedback

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -30,7 +30,7 @@ const Page = async ({ params }: RouteParams) => {
         <p className='bg-dark-200 px-4 py-2 rounded-lg h-fit capitalize'>{interview.type}</p>
       </div>
 
-      <Agent userName={user?.name} type={user?.id} interviewId={id} type="interview" questions={interview.questions}/> 
+      <Agent userName={user?.name || ""} userId={user?.id} interviewId={id} type="interview" questions={interview.questions}/> 
     </>
   )
 }
--- components/Agent.tsx
+++ components/Agent.tsx
@@ -6,6 +6,7 @@ import { cn } from '@/utils';
 import { useRouter } from 'next/navigation';
 import { vapi } from '@/lib/vapi.sdk';
 import { interviewer } from '@/constants';
+import { createFeedback } from '@/lib/acitons/general.action';
 
 
 enum CallStatus {
@@ -66,10 +67,11 @@ const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) =
     const handleGenerateFeedback = async (messages: SavedMessage[]) => {
         console.log("generate feedback here")
 
-        const { success, id} = {
-            success: true,
-            id: "feedback-id"
-        }
+        const { success, feedbackId: id} = await createFeedback({
+            interviewId: interviewId!,
+            userId: userId!,
+            transcript: messages
+        })
 
         if (success && id) {
             router.push(`/interview/${interviewId}/feedback`);
