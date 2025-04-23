f9199bfd459d4538eabc74e741ef6ed78436911e
selim <selimb768@gmail.com>
2025-04-23 19:46:14 +0300

    handlecall

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -154,14 +154,14 @@ const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) =
 
     <div className='w-full flex justify-center'>
         {callStatus !== "ACTIVE" ? (
-            <button className='relative btn-call' onClick={handleCall}>
+            <button className='relative btn-call' onClick={() => handleCall()}>
                 <span className={cn("absolute animate-ping rounded-full opacity-75", callStatus !== "CONNECTING" && "hidden")} />
                 <span>
                     {isCallInactiveOrFinished? "Call"  : ". . ."}
                 </span>
             </button>
         ) : (
-            <Button className='btn-disconnect' onClick={handleDisconnect}>
+            <Button className='btn-disconnect' onClick={() => handleDisconnect()}>
                 End
             </Button>
         )}
