d21a1a53f2e0dee161b4a9048d0a03c78a88c419
selim <selimb768@gmail.com>
2025-04-24 11:40:37 +0300

    firebase changes

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoc296UjYbJsho0ufBk7v-TI25z-u4lvU",
-  authDomain: "prepwise-de08d.firebaseapp.com",
-  projectId: "prepwise-de08d",
-  storageBucket: "prepwise-de08d.firebasestorage.app",
-  messagingSenderId: "214440683286",
-  appId: "1:214440683286:web:cd0db251ed2afc0c61d237",
-  measurementId: "G-YGEL4Y8NV9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
